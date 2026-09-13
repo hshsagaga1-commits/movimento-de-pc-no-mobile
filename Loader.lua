b6612b0d994dab3e35b561801458880ba10d3f63
hshsagaga1-commits <hshsagaga1@gmail.com>
2026-09-13 20:38:07 -0300

    Add cache-busting loader

---
+++ Loader.lua
@@ -0,0 +1,14 @@
+local HttpService = game:GetService("HttpService")
+local url = "https://raw.githubusercontent.com/hshsagaga1-commits/movimento-de-pc-no-mobile/main/PCMovement.lua"
+
+local source = game:HttpGet(
+    url .. "?_cb=" .. HttpService:GenerateGUID(false),
+    true
+)
+
+local chunk, err = loadstring(source)
+if not chunk then
+    error(err)
+end
+
+return chunk()

fd6c240dfb32dd4c466e9834c5c5181e7f354aeb
hshsagaga1-commits <hshsagaga1@gmail.com>
2026-09-14 08:46:54 -0300

    Point loader to V18 final measured polish

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,5 @@
 local HttpService = game:GetService("HttpService")
-local url = "https://raw.githubusercontent.com/hshsagaga1-commits/movimento-de-pc-no-mobile/main/PCMovementV17.lua"
+local url = "https://raw.githubusercontent.com/hshsagaga1-commits/movimento-de-pc-no-mobile/main/PCMovementV18.lua"
 
 local source = game:HttpGet(
     url .. "?_cb=" .. HttpService:GenerateGUID(false),

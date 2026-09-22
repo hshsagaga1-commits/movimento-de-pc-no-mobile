042ec2542151b28c654f3a80aef5b3dabccf239e
hshsagaga1-commits <hshsagaga1@gmail.com>
2026-09-22 08:36:38 -0300

    chore: add organized sensitivity precision entry

---
+++ Organizado/Separado/Sensibilidade_Precisao.lua
@@ -0,0 +1,7 @@
+-- Separado - Sensibilidade + Precisao
+local HttpService=game:GetService("HttpService")
+local url="https://raw.githubusercontent.com/hshsagaga1-commits/movimento-de-pc-no-mobile/rebuild-zero-v1/Loader_OverhaulCameraV1.lua"
+local source=game:HttpGet(url.."?_cb="..HttpService:GenerateGUID(false),true)
+local chunk,err=loadstring(source)
+if not chunk then error("[Separado - Sensibilidade + Precisao] "..tostring(err)) end
+return chunk()
